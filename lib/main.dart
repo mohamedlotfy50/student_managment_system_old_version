3211af74172a7efddaee71e54cfa8680bf650afd
Mohamed Lotfy <molotfy50@gmail.com>
2021-01-29 01:12:43 +0200

    adding firebase packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'injection.dart';
 import 'presentation/core/widget/my_app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   configureInjection();
   runApp(MyApp());
 }
